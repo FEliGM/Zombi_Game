f56a686f6f6a3d7b583b5b220ea2be5673cbfc0b
elizabethcideb@gmail.com <elizabethcideb@gmail.com>
2020-08-20 09:21:33 -0500

    toys start

---
+++ Zombie Game/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float raycastDistance = 1f;
+    public LayerMask raycastMarsk;
+
+    Rigidbody rb;
+    
+    Vector3 input;
+    Camera cachedCamera;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        cachedCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        input.x = h;
+        input.z = v;
+
+        Ray ray = cachedCamera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(Vector3.zero, Vector3.right, out hit, raycastDistance))
+        {
+            Vector3 hitPoint = hit.point;
+            hitPoint.y = transform.position.y;
+            transform.LookAt(hitPoint);
+        }
+        Debug.DrawRay(Vector3.zero, Vector3.right * raycastDistance, Color.red);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = input * speed;
+        rb.velocity = velocity;
+    }
+}
